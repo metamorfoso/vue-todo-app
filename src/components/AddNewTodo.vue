17f382ed103537a21795293704cae7d9d47c873e
Ivan Ignatov <ivanign04.alt@gmail.com>
2024-03-13 20:12:39 +1300

    Separate component for inputs for adding a new todo item.

---
+++ src/components/AddNewTodo.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  const emit = defineEmits<{
+    (e: 'newTodoTextAdded', text: string): void
+  }>()
+
+  const newTodoText = defineModel({ default: '' })
+
+  const addTodo = () => {
+    emit('newTodoTextAdded', newTodoText.value)
+    newTodoText.value = ''
+  }
+</script>
+
+<template>
+  <input v-model.trim="newTodoText" type="text" @keyup.enter="addTodo" />
+  <button @click="addTodo">Add todo</button>
+</template>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,23 +1,17 @@
 <script setup lang="ts">
   import { ref } from 'vue'
-
-  type Todo = {
-    id: number
-    text: string
-    isDone: boolean
-  }
+  import type { Todo } from '../types'
+  import AddNewTodo from './AddNewTodo.vue';
 
   const props = defineProps<{
     initialTodos?: Todo[]
   }>()
 
   const todos = ref<Todo[]>(props.initialTodos || [])
-  const newTodoText = defineModel({ default: '' })
 
-  const addTodo = () => {
+  const addTodo = (newTodoText: string) => {
     const lastId = todos.value?.[todos.value.length - 1]?.id || 0
-    todos.value.push({ id: lastId + 1, text: newTodoText.value, isDone: false })
-    newTodoText.value = ''
+    todos.value.push({ id: lastId + 1, text: newTodoText, isDone: false })
   }
 
   const removeTodo = (id: number) => {
@@ -26,10 +20,7 @@
 </script>
 
 <template>
-  <div>
-    <input v-model.trim="newTodoText" type="text" @keyup.enter="addTodo" />
-    <button @click="addTodo">Add todo</button>
-  </div>
+  <AddNewTodo @new-todo-text-added="addTodo" />
 
   <ul>
     <li v-for="todo in todos">
